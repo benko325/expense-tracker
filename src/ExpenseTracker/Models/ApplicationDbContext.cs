a074744cb385949b6d8bb5b68c9deb55d5fdc197
Benjamin Havlik <514493@mail.muni.cz>
2023-12-03 16:29:00 +0100

    feat: db setup

---
+++ src/ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
